94512d4bf58b863afc87d628420153c26cc5eba5
Yana <yana.skrepchuk@yandex.ru>
2022-12-13 15:16:30 +0500

    new2

--- FWCS/Program.cs
+++ FWCS/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.Clear();
+
+string[] array = AskArray();
+string[] AskArray() 
+{
+    Console.WriteLine("Введите массив строк (например: sd, :), 12345):");
+    return Console.ReadLine().Split(", ");
+}
+            var result = new string[array.Length];            
+            var realSize = 0;          
+            foreach (var i in array)
+            {
+                if (i.Length <= 3)
+                {
+                    result[realSize] = i;
+                    realSize++;
+                }
+            }
+            Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result.Where(x => x != null))}]");
